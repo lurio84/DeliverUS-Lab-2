1f4ea8bdaf7ac89bcc28ffb6481680075cdab23f
lurio84 <lucas.ronquillo@gmail.com>
2024-03-08 15:40:45 +0100

    Exercises done

--- DeliverUS-Backend/src/controllers/RestaurantController.js
+++ DeliverUS-Backend/src/controllers/RestaurantController.js
@@ -22,19 +22,63 @@ const index = async function (req, res) {
 // TODO: Complete the following functions
 
 const create = async function (req, res) {
-
+  const newRestaurant = Restaurant.build(req.body)
+  newRestaurant.userId = 1
+  try {
+    const restaurant = await newRestaurant.save()
+    res.json(restaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
-const show = async function (req, res) {
-
+const newLocal = async function (req, res) {
+  try {
+    const restaurant = await Restaurant.findByPk(req.params.restaurantId, {
+      attributes: { exclude: ['userId'] },
+      include: [{
+        model: Product,
+        as: 'products',
+        include: { model: ProductCategory, as: 'productCategory' }
+      },
+      {
+        model: RestaurantCategory,
+        as: 'restaurantCategory'
+      }],
+      order: [[{ model: Product, as: 'products' }, 'order', 'ASC']]
+    }
+    )
+    res.json(restaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
+const show = newLocal
 
 const update = async function (req, res) {
-
+  try {
+    const restaurant = await Restaurant.findByPk(req.params.restaurantId)
+    if (!restaurant) {
+      return res.status(404).json({ error: 'Restaurant not found' })
+    }
+    await restaurant.update(req.body)
+    res.json(restaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const destroy = async function (req, res) {
-
+  try {
+    const restaurant = await Restaurant.findByPk(req.params.restaurantId)
+    if (!restaurant) {
+      return res.status(404).json({ error: 'Restaurant not found' })
+    }
+    await restaurant.destroy()
+    res.json({ message: 'Restaurant deleted successfully' })
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const RestaurantController = {
